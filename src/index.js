4f311777a889bd808fc4b3169835e63612fbcf07
dylan1607 <dylan@dylans-MacBook-Pro.local>
2021-09-08 00:51:09 +0700

    config AWS amplify

--- src/index.js
+++ src/index.js
@@ -5,6 +5,12 @@ import App from "./App";
 import { store } from "./app/store";
 import { Provider } from "react-redux";
 
+//Setup Connect Amplify
+import Amplify from "aws-amplify";
+import awsExports from "./aws-exports";
+Amplify.configure(awsExports);
+//---------------------//
+
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
